dadedd15708e4fcc374e5c9dea85bf65350575f9
Eric Arndt <earndt1701@gmail.com>
2026-01-08 05:00:00 -0800

    Working todo list, checkboxes and delete

--- W1/react-todo-list/src/App.jsx
+++ W1/react-todo-list/src/App.jsx
@@ -16,6 +16,25 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ]
     })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => { 
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
@@ -29,14 +48,15 @@ export default function App() {
         </form>
         <h1 className="header">Todo List</h1>
         <ul className="list">
+          {todos.length === 0 && "No todos yet"}
           {todos.map(todo => {
             return (
             <li key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed} />
+              <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
           </li>
             )
           })}
