2ba42b05b95994296020c9376db6cc582bd1e2ba
Eric Arndt <earndt1701@gmail.com>
2026-01-08 18:23:28 -0800

    VSC Errors persist, total ridiculous

--- W1/react-todo-list/src/App.jsx
+++ W1/react-todo-list/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./styles.css"
 import { NewTodoForm } from "./NewTodoForm"
 import { TodoList } from "./TodoList"
--- W1/react-todo-list/src/TodoList.jsx
+++ W1/react-todo-list/src/TodoList.jsx
@@ -3,12 +3,17 @@ import { TodoItem } from "./TodoItem"
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
-        {todos.length === 0 && "No todos yet"}
-        {todos.map(todo => {
-          return (
-            <TodoItem {...todo} key={todo.id} toggleTodo={todo.toggleTodo} deleteTodo={todo.deleteTodo}/>   
-            )
-        })}
-      </ul> 
-    )
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
+  )
 }
